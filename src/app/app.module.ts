bb1af5e3ea8704e511ebf6b4b4e40feaedf3a81b
Nima <nimappl@hotmail.com>
2020-10-21 10:06:21 +0330

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ShoppingListComponent } from './Components/shopping-list/shopping-list.component';
+import { ShoppingListItemComponent } from './Components/shopping-list/shopping-list-item/shopping-list-item.component';
+import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipesComponent } from './Components/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListItemComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
