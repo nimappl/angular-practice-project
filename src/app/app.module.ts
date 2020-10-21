27839bc0a0cb41e651990339620125b4b43a0e41
Nima <nimappl@hotmail.com>
2020-10-21 12:47:58 +0330

    Header constructed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { ShoppingListComponent } from './Components/shopping-list/shopping-list.component';
-import { ShoppingListItemComponent } from './Components/shopping-list/shopping-list-item/shopping-list-item.component';
 import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './Components/recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipesComponent } from './Components/recipes/recipes.component';
+import { RecipeDetailComponent } from './Components/recipes/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './Components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListItemComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeDetailComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
